26e4ddc51b60a3df43ecc0ca8060b11fc458e338
Gabriel de Carvalho <gabriel.decarvalho-ext@block.one>
2020-04-09 21:40:41 -0500

    add QuickSort and empty scripts

---
+++ quickSort.js
@@ -0,0 +1,27 @@
+const quickSort = (array) => {
+  if (array.length < 2) return array
+
+  const pivot = array[0]
+  array.splice(0, 1)
+
+  const less = array.filter((e) => e <= pivot)
+  const greater = array.filter((e) => e > pivot)
+
+  return [...quickSort(less), pivot, ...quickSort(greater)]
+}
+
+const arrays = [10, 30, 50, 100, 1000].map((n) => [
+  ...new Set(
+    [...Array(n).keys()].map((_) => Math.round(Math.random() * n * 10 + 1))
+  ),
+])
+
+console.log('Sorting arrays:\n------\n')
+arrays.forEach((array) => {
+  console.log('------')
+  console.log(`Sorting array with: ${array.length} elements`)
+  console.time()
+  quickSort(array)
+  console.timeEnd()
+  console.log(`------\n\n`)
+})
