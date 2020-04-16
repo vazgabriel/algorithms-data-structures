c651255b550b041e3072c4a47251234496473779
Gabriel de Carvalho <gabriel.decarvalho-ext@block.one>
2020-04-16 07:49:28 -0500

    add comments

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -20,6 +20,18 @@ const isValidBST = (root) => {
   return helper(root, -Infinity, Infinity)
 }
 
+/*
+  10 \/
+    R: (15, 10, Infinity) \/
+      R: (null, 15, Infinity) -> true
+      L: (11, 10, 15) \/
+        R: (null, 11, 15)     -> true
+        L: (9, 10, 11)        -> false
+    L: (5, -Infinity, 10) \/
+      R: (null, 5, Infinity)  -> true
+      L: (null, -Infinity, 5) -> true
+ */
+
 const node = new TreeNode(10)
 node.left = new TreeNode(5)
 node.right = new TreeNode(15)
@@ -27,7 +39,12 @@ node.right = new TreeNode(15)
 /// True
 console.log(isValidBST(node))
 
-node.right.left = new TreeNode(9)
+node.right.left = new TreeNode(11)
+
+/// True
+console.log(isValidBST(node))
+
+node.right.left.left = new TreeNode(9)
 
 /// False
 console.log(isValidBST(node))
